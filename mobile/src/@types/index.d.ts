c1c021ebac62e41389cfcda968a26088602829ae
Saulo Braine <saulo@braine.dev>
2020-08-19 19:42:46 -0300

    Adding react-native-dotenv

--- mobile/src/@types/index.d.ts
+++ mobile/src/@types/index.d.ts
@@ -1,1 +1,6 @@
 declare module '*.png';
+
+declare module 'react-native-dotenv' {
+  export const API_URL: string;
+  export const ENV: 'dev' | 'prod';
+}
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: 'https://saulobraine-proffy-server.herokuapp.com'
+  baseURL: API_URL || 'http://localhost:3333'
 });
 
 export default api;
